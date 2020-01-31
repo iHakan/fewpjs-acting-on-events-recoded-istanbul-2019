4145ba3ade59545ea5a2a2a827c9e2b42352208f
Hakan Yildiz <hakan@re-coded.com>
2020-01-31 14:05:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // Your code here
 let dodger = document.getElementById('dodger');
+
+function moveDodgerLeft(){
+  let LeftNumbers = dodger.style.left.replace('px','');
+  let left = parseInt(LeftNumbers, 10);
+  
+  if(left > 0){
+    dodger.style.left = `${left-1}px`;
+  }
+}
